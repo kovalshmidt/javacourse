8c97615ba7de2a263acf6e0262d7435ff65cb6b8
Martin Nebeling <martneb@aol.com>
2020-04-03 19:14:45 +0200

    created dog class

---
+++ src/main/java/Lesson_1/Dog.java
@@ -0,0 +1,57 @@
+package Lesson_1;
+
+//Formatierung: Strg+Alt+L
+public class Dog {
+    public String colorOfTheSkin;
+    public int weight;
+    public int maxSpeed;
+    public int age;
+    public int height;
+    public String gender;
+    public String race;
+    public String name;
+
+    /**
+     * This a constructor that sets up all the fields
+     */
+    public Dog(String color, int weight, int maxSpeed, int age, int height, String gender, String race,String name) {
+        this.colorOfTheSkin = color;
+        this.weight = weight;
+        this.maxSpeed = maxSpeed;
+        this.age = age;
+        this.height = height;
+        this.gender = gender;
+        this.race = race;
+        this.name = name;
+    }
+
+    /**
+     * This is a default constructor (Dobermann)
+     */
+    public Dog() {
+        this.colorOfTheSkin = "Black";
+        this.weight = 40;
+        this.maxSpeed = 56;
+        this.age = 6;
+        this.height = 68;
+        this.gender = "Male";
+        this.race = "Dobermann";
+        this.name = "Dog Doe";
+    }
+
+    /**
+     * This method calculate the dog age into human age
+     */
+    public int getHumanAge() {
+        return this.age * 7;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String dogName) {
+        this.name = dogName;
+
+    }
+}
---
+++ src/main/java/Lesson_1/Main.java
@@ -0,0 +1,12 @@
+package Lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dobermann = new Dog();
+        Dog germanShepard = new Dog("Brown", 30, 50, 8, 60, "Female", "German Shepard","Josie");
+        System.out.println("This dog's name is, "+dobermann.getName());
+        dobermann.setName("Boris");
+        System.out.println("This dog's name is, "+dobermann.getName());
+    }
+
+}
