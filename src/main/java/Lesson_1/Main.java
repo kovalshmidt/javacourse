de3711dc85f677d9157123d05b43f4d09a76718e
Martin Nebeling <martneb@aol.com>
2020-04-03 19:36:44 +0200

    added screenprint out for Dog to human age

--- src/main/java/Lesson_1/Main.java
+++ src/main/java/Lesson_1/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Dog dobermann = new Dog();
         Dog germanShepard = new Dog("Brown", 30, 50, 8, 60, "Female", "German Shepard","Josie");
-        System.out.println("This dog's name is, "+dobermann.getName());
+        //System.out.println("This dog's name is, "+dobermann.getName());
         dobermann.setName("Boris");
-        System.out.println("This dog's name is, "+dobermann.getName());
+       // System.out.println("This dog's name is, "+dobermann.getName());
+       int shepardAge = germanShepard.getHumanAge();
+        System.out.println("The dog's age in human years would be: "+shepardAge);
+        int dobermannAge = dobermann.getHumanAge();
+        System.out.println("The dog's age in human years would be: "+dobermannAge);
     }
 
 }
