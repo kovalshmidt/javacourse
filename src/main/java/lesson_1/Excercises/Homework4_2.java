91b3a899f5abf3d9df3dbb6f6388db179e53e60f
Martin Nebeling <martneb@aol.com>
2020-04-06 10:13:17 +0200

    first 2 exercises of  #4 are are completed

---
+++ src/main/java/lesson_1/Excercises/Homework4_2.java
@@ -0,0 +1,21 @@
+package Lesson_1.Excercises;
+
+import java.util.Scanner;
+
+public class Homework4_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1, number2;
+        int biggerOne;
+        System.out.println("The first number is:");
+        number1 = input.nextInt(); //Input first Int
+        System.out.println("The second number is:");
+        number2 = input.nextInt(); //Input second Int
+        if (number1 > number2)
+            System.out.println(number1 + " is bigger then "+ number2);
+        if (number1 < number2)
+            System.out.println(number1 + " is smaller then "+ number2);
+        if (number1 == number2)
+            System.out.println("These numbers are equal");
+    }
+}
