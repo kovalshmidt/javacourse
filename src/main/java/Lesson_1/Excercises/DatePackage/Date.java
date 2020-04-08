402d06705b1dee5e09437700f7bbe26fe054f798
Martin Nebeling <martneb@aol.com>
2020-04-08 10:18:14 +0200

    finished Date project

---
+++ src/main/java/Lesson_1/Excercises/DatePackage/Date.java
@@ -0,0 +1,47 @@
+package Lesson_1.Excercises.DatePackage;
+
+public class Date {
+    public int day;
+    public int month;
+    public int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public Date() {
+        this.day = 1;
+        this.month = 1;
+        this.year = 1970;
+    }
+
+    public void setDay(int input) {
+        this.day = input;
+    }
+
+    public void setMonth(int input) {
+        this.month = input;
+    }
+
+    public void setYear(int input) {
+        this.year = input;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String displayDate(){
+        return this.day+"/"+this.month+"/"+this.year;
+    }
+}
---
+++ src/main/java/Lesson_1/Excercises/DatePackage/DateTest.java
@@ -0,0 +1,28 @@
+package Lesson_1.Excercises.DatePackage;
+
+import java.util.Scanner;
+
+public class DateTest {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    Date givenDate = new Date();
+
+        System.out.println("Please type the day in: ");
+        givenDate.setDay(input.nextInt());
+        System.out.println("Please type the month in: ");
+        givenDate.setMonth(input.nextInt());
+        System.out.println("Please type the year in:");
+        givenDate.setYear(input.nextInt());
+
+        int givenDay = givenDate.getDay();
+        System.out.println("The given day is: "+givenDay);
+        int givenMonth = givenDate.getMonth();
+        System.out.println("The given day is: "+givenMonth);
+        int givenYear = givenDate.getYear();
+        System.out.println("The given day is: "+givenYear);
+
+        String last = givenDate.displayDate();
+        System.out.println("The date you gave is: "+ last);
+    }
+}
