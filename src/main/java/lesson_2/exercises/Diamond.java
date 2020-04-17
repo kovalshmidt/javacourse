4a8d9c76697590bcd78da2ca8d0b690bd86307c7
Martin Nebeling <martneb@aol.com>
2020-04-17 09:51:03 +0200

    finished.resolves #22

--- src/main/java/lesson_2/exercises/Diamond.java
+++ src/main/java/lesson_2/exercises/Diamond.java
@@ -3,6 +3,7 @@ package lesson_2.exercises;
 
 /**
  * Goal:
+<<<<<<< HEAD
  *     *
  *    ***
  *   *****
@@ -12,10 +13,22 @@ package lesson_2.exercises;
  *   *****
  *    ***
  *     *
+=======
+ *         *
+ *       * * *
+ *     * * * * *
+ *   * * * * * * *
+ * * * * * * * * * *
+ *   * * * * * * *
+ *     * * * * *
+ *       * * *
+ *         *
+>>>>>>> 8dca059... finished.resolves #22
  */
 public class Diamond {
     public static void main(String[] args) {
         for (int y = 1; y <= 9; y++) {
+<<<<<<< HEAD
             for (int x = 1; x <= 9; x++) {
                 if ((((3 <= y) && (y <= 7)) && ((3 <= x) && (x <= 7))) || (((4 <= y) && (y <= 6)) &&
                         ((2 <= x) && (x <= 8))) || (((2 <= y) && (y <= 8)) && ((4 <= x) && (x <= 6))) || (x == 5) || (y == 5)) {
@@ -24,6 +37,14 @@ public class Diamond {
                     System.out.print(" ");
                 }
                 if (x == 9) {
+=======
+            for (int x = 1; x <= 9; x++){
+                if ((((3<=y)&&(y<=7))&&((3<=x)&&(x<=7)))||(((4<=y)&&(y<=6))&&((2<=x)&&(x<=8)))||(((2<=y)&&(y<=8))&&((4<=x)&&(x<=6)))||(x == 5)||(y ==5)){
+                System.out.print("* ");}else{
+                    System.out.print("  ");
+                }
+                if (x == 9){
+>>>>>>> 8dca059... finished.resolves #22
                     System.out.println();
                 }
             }
