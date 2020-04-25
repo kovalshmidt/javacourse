23730b6233cbad8eca9f298ee7e36761786cf1fd
Martneb <63070087+Martneb@users.noreply.github.com>
2020-04-25 18:38:35 +0200

    Homework (#41)
    * finished.resolves #22
    
    * general commit, utils class
    
    * finished.resolves #28 #29 #30 #31
    
    * finished.resolves #32
    
    * finished.resolves #33
    
    * finished.resolves #34
    
    * finished.resolves #35
    
    * fixed for requested changes
    
    * finished.resolves #36
    
    * optimized.resolves #36
    
    * finished.resolves #37
    
    * minor fix
    bounds now 1-1000 instead of 0-999
    
    * fixes.resolves #40
    
    * finished.resolves #38
    
    * finished.resolves #38
    
    * finished.resolves #38
    
    * finished.resolves #39
    added TurtleGraphicsDIY, a painter tool
    
    * fixed turtle going out of bounds #39

---
+++ src/main/java/lesson_3/exercises/Turtle.java
@@ -0,0 +1,160 @@
+package lesson_3.exercises;
+
+public class Turtle {
+    private boolean pen;
+    private int direction;
+    private int y, x;
+
+    public Turtle(boolean pen, int direction, int y, int x) {
+        this.pen = pen;
+        this.direction = direction;
+        this.y = y;
+        this.x = x;
+
+    }
+
+    public Turtle() {
+        this.pen = false;
+        this.direction = 1;
+        this.y = 0;
+        this.x = 0;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void addY() {
+        this.y++;
+        if (this.y > 19) {
+            this.y = 19;
+        }
+    }
+
+    public void addX() {
+        this.x++;
+        if (this.x > 19) {
+            this.x = 19;
+        }
+    }
+
+    public void subtractY() {
+        this.y--;
+        if (this.y < 0) {
+            this.y = 0;
+        }
+    }
+
+    public void subtractX() {
+        this.x--;
+        if (this.x < 0) {
+            this.x = 0;
+        }
+    }
+
+    public boolean getPen() {
+        return this.pen;
+    }
+
+    public int getDirection() {
+        return this.direction;
+    }
+
+    public void switchPen() {
+        this.pen = !pen;
+    }
+
+    //1 = right, 2 = down, 3 = left, 4 = up
+    public void turnRight() {
+        switch (direction) {
+            case 1:
+                this.direction = 2;
+                break;
+            case 2:
+                this.direction = 3;
+                break;
+            case 3:
+                this.direction = 4;
+                break;
+            case 4:
+                this.direction = 1;
+        }
+    }
+
+    public void turnLeft() {
+        switch (direction) {
+            case 1:
+                this.direction = 4;
+                break;
+            case 2:
+                this.direction = 1;
+                break;
+            case 3:
+                this.direction = 2;
+                break;
+            case 4:
+                this.direction = 3;
+        }
+
+    }
+    //Main block
+
+    public static void executeOrder(int[][] canvas, Turtle painter, int order, int length) {
+        switch (order) {
+            case 1:
+            case 2:
+                painter.switchPen();
+                break;
+            case 3:
+                painter.turnRight();
+                break;
+            case 4:
+                painter.turnLeft();
+                break;
+            case 5:
+                for (int l = 0; l <= length; l++) {
+                    if (painter.getPen()) {
+                        canvas[painter.getY()][painter.getX()] = 1;
+                    }
+                    switch (painter.getDirection()) {
+                        case 1:
+                            painter.addX();
+                            break;
+                        case 2:
+                            painter.addY();
+                            break;
+                        case 3:
+                            painter.subtractX();
+                            break;
+                        case 4:
+                            painter.subtractY();
+                            break;
+                    }
+                }
+                break;
+            case 6:
+                for (int y = 0; y <= 19; y++) {
+                    for (int x = 0; x <= 19; x++) {
+                        if (canvas[x][y] == 1) {
+                            System.out.print("*  ");
+                        } else {
+                            System.out.print("   ");
+                        }
+                        if (x == 19) {
+                            System.out.println();
+                        }
+                    }
+                }
+                break;
+            case 9:
+                System.exit(0);
+                break;
+        }
+    }
+
+
+}
---
+++ src/main/java/lesson_3/exercises/TurtleGraphics.java
@@ -0,0 +1,87 @@
+package lesson_3.exercises;
+
+/**
+ * The Logo language made the concept of turtle graphics famous. Imagine a mechanical turtle
+ * that walks around the room under the control of a Java application. The turtle holds
+ * a pen in one of two positions, up or down. While the pen is down, the turtle traces out
+ * shapes as it moves, and while the pen is up, the turtle moves about freely without writing
+ * anything. In this problem, you’ll simulate the operation of the turtle and create a
+ * computerized sketchpad.
+ * Use a 20-by-20 array floor that’s initialized to zeros. Read commands from an array that
+ * contains them. Keep track of the current position of the turtle at all times and whether
+ * the pen is currently up or down. Assume that the turtle always starts at position (0, 0) of
+ * the floor with its pen up. The set of turtle commands your application must process are
+ * shown in the image:
+ * <p>
+ * Command  ||  Meaning
+ * 1            Pen up
+ * 2            Pen down
+ * 3            Turn right
+ * 4            Turn left
+ * 5,X          Move forward by X spaces
+ * 6            Display array
+ * 9            End of data (sentinel)
+ * <p>
+ * Suppose that the turtle is somewhere near the center of the floor. The following
+ * “program”
+ * would draw and display a 12-by-12 square, leaving the pen in the up position:
+ * 2
+ * 5,12
+ * 3
+ * 5,12
+ * 3
+ * 5,12
+ * 3
+ * 5,12
+ * 1
+ * 6
+ * 9
+ * As the turtle moves with the pen down, set the appropriate elements of array floor to 1s.
+ * When the 6 command (display the array) is given, wherever there’s a 1 in the array,
+ * display an asterisk or any character you choose. Wherever there’s a 0, display a blank.
+ * Write an application to implement the turtle graphics capabilities discussed here. Write
+ * several turtle graphics programs to draw interesting shapes. Add other commands to
+ * increase the power of your turtle graphics language.
+ */
+
+
+public class TurtleGraphics {
+    public static void main(String[] args) {
+        int[][] canvas = new int[20][20];
+        Turtle painter = new Turtle();
+        int order, length = 0;
+        for (int y = 0; y <= 19; y++) {
+            for (int x = 0; x <= 19; x++) {
+                canvas[y][x] = 0;
+            }
+        }
+        order = 2;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 5;
+        length = 12;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 3;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 5;
+        length = 12;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 3;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 5;
+        length = 12;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 3;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 5;
+        length = 12;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 1;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order=3;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 6;
+        Turtle.executeOrder(canvas, painter, order, length);
+        order = 9;
+        Turtle.executeOrder(canvas, painter, order, length);
+    }
+}
