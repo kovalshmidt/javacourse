70e23540d8ac4fa16e60889b6013924204b7e70a
Martin Nebeling <martneb@aol.com>
2020-05-28 12:55:42 +0200

    finished.resolved #60

---
+++ src/main/java/lesson_5/exercises/PrimeNumber.java
@@ -0,0 +1,58 @@
+package lesson_5.exercises;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Collection;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LinkedList<Integer> primeFactors = new LinkedList<>();
+        boolean isPrime = true;
+        int factor;
+
+        System.out.println("Type in a number");
+        final int Prime = input.nextInt();
+
+        factor = primeCheck(Prime);
+        if (factor != 0) {
+            isPrime = false;
+            primeFactors.add(factor);
+            factorCheck(primeFactors, Prime, factor);
+        }
+
+        System.out.println(Prime + " is " + (isPrime ? "" : "not") + " a prime number.");
+        if (primeFactors.size() > 0) {
+            Collection<Integer> setFactors = new LinkedHashSet<>(primeFactors);
+            System.out.println(setFactors + " are the prime factors.");
+        }
+    }
+
+    //Returns lowest prime factor
+    private static int primeCheck(int prime) {
+        for (int isDivisible = 2; isDivisible < prime; isDivisible++) {
+            if (prime % isDivisible == 0) {
+                return isDivisible;
+            }
+        }
+        return 0;
+    }
+
+    //returns all prime factors
+    private static LinkedList<Integer> factorCheck(LinkedList<Integer> primeFactors, int prime, int factor) {
+        //other factor (e.g. 2*50 = 100 >> 100/2 = 50)
+        int otherFactor = prime / factor;
+        //lowest prime 2 of 50 >> 2*2*25 >> 2*2*5*5
+        int lowerFactor = primeCheck(otherFactor);
+        if (lowerFactor != 0) {
+            primeFactors.add(lowerFactor);
+            factorCheck(primeFactors, otherFactor, lowerFactor);
+        } else {
+            primeFactors.add(otherFactor);
+        }
+
+
+        return primeFactors;
+    }
+}
