ff4031a36a3a93170841e5b8c5542e5f2be1f01f
Martin Nebeling <martneb@aol.com>
2020-06-04 09:39:47 +0200

    added description.resolved #69.6

--- src/main/java/lesson_6/exercises/Euler/SumSquareDifference.java
+++ src/main/java/lesson_6/exercises/Euler/SumSquareDifference.java
@@ -1,4 +1,30 @@
 package lesson_6.exercises.Euler;
 
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ * <p>
+ * 1^2+2^2+...+102=385
+ * The square of the sum of the first ten natural numbers is,
+ * <p>
+ * (1+2+...+10)^2=55^2=3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+ * <p>
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+
 public class SumSquareDifference {
+    public static void main(String[] args) {
+        int sumOfSquare = 0, squareOfSum = 0,difference;
+        for (int m = 1; m <= 100; m++) {
+            sumOfSquare += Math.pow(m,2);
+            squareOfSum += m;
+        }
+        squareOfSum = (int) Math.pow(squareOfSum,2);
+        System.out.println("Sum of the square: " + sumOfSquare);
+        System.out.println("Square of the sum: " + squareOfSum);
+        difference = squareOfSum -sumOfSquare;
+        System.out.println("Difference: " + difference);
+
+    }
+
 }
