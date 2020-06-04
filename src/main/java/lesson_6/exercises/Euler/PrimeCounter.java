5cf9dcdcbb256fc9cfee79e8f4e53ed904a5b904
Martin Nebeling <martneb@aol.com>
2020-06-04 09:59:51 +0200

    added description.resolved #69.7

--- src/main/java/lesson_6/exercises/Euler/PrimeCounter.java
+++ src/main/java/lesson_6/exercises/Euler/PrimeCounter.java
@@ -1,6 +1,10 @@
 package lesson_6.exercises.Euler;
 
-import lesson_5.exercises.PrimeNumber;
+/**
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ */
 
 public class PrimeCounter {
     public static void main(String[] args) {
