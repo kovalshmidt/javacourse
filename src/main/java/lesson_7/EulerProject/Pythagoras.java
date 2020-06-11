7d5b5c631f81e6f6215b413e811fececf206b84a
Martin Nebeling <martneb@aol.com>
2020-06-11 12:47:08 +0200

    finished.resolved #70 9 and 10

--- src/main/java/lesson_7/EulerProject/Pythagoras.java
+++ src/main/java/lesson_7/EulerProject/Pythagoras.java
@@ -1,5 +1,15 @@
 package lesson_7.EulerProject;
 
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a2 + b2 = c2
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
 public class Pythagoras {
     public static void main(String[] args) {
         int numberOne = 0, numberTwo = 0, numberThree = 0;
--- src/main/java/lesson_7/EulerProject/SummationOfPrimes.java
+++ src/main/java/lesson_7/EulerProject/SummationOfPrimes.java
@@ -1,5 +1,11 @@
 package lesson_7.EulerProject;
 
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
 import lesson_5.exercises.PrimeNumber;
 
 import java.util.HashSet;
