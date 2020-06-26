1fec7f876ac4b7c56bd2adcd1095fcd4871fae7b
Martin Nebeling <martneb@aol.com>
2020-06-26 11:08:31 +0200

    finished.resolved #76

---
+++ src/main/java/lesson_9/exercises/homework/OrderOfCatchBlocks.java
@@ -0,0 +1,43 @@
+package lesson_9.exercises.homework;
+
+import lesson_9.exercises.exceptions.ExceptionA;
+import lesson_9.exercises.exceptions.ExceptionB;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class OrderOfCatchBlocks {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+
+        try {
+            number = Integer.parseInt(input.next());
+            if (number > 5 && number <= 10)
+                throwsExceptionB();
+            if (number > 10)
+                throwsExceptionA();
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("please type in something!");
+            //SuperClass ExceptionA before ExceptionB
+        } catch (ExceptionA exA) {
+            System.out.println("Number is higher than 10");
+            //ExceptionB already caught by ExceptionA
+        } catch (ExceptionB exB) {
+            System.out.println("Number is higher than 5");
+        } catch (IOException ioException) {
+            ioException.getLocalizedMessage();
+        } finally {
+            System.out.println("Number is " + number);
+        }
+    }
+
+    public static void throwsExceptionA() throws Exception {
+        throw new ExceptionA();
+    }
+
+    public static void throwsExceptionB() throws Exception {
+        throw new ExceptionB();
+    }
+
+}
