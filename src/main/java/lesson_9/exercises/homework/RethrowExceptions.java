b5e5f9659721c47f4cd02cebee886b58c417d6e1
Martin Nebeling <martneb@aol.com>
2020-07-14 16:16:53 +0200

    finished.resolved #78

---
+++ src/main/java/lesson_9/exercises/homework/RethrowExceptions.java
@@ -0,0 +1,24 @@
+package lesson_9.exercises.homework;
+
+public class RethrowExceptions {
+    public static void SomeMethod() throws Exception{
+        System.out.println("SomeMethod()");
+        throw new Exception("thrown from SomeMethod()");
+    }
+    public static void SomeMethod2() throws Throwable{
+        try{
+            SomeMethod();
+        }catch (Exception e){
+            System.out.println("SomeMethod2()");
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        try{
+            SomeMethod2();
+        }catch (Exception e){
+            System.out.println("Main");
+        }
+    }
+}
