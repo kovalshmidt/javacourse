31a0bae81d19f353fc5162db4f0b003fa83b803f
Martin Nebeling <martneb@aol.com>
2020-07-24 18:35:30 +0200

    finished.resolved #82

---
+++ src/main/java/lesson_9/exercises/homework/Pair.java
@@ -0,0 +1,34 @@
+package lesson_9.exercises.homework;
+
+/**
+ *Write a generic class Pair which has two type parameters—F and S— each representing the type of the first and second
+ * element of the pair, respectively. Add get and set methods for the first and second elements of the pair. [Hint: The
+ * class header should be public class Pair<F, S>.]
+ */
+
+public class Pair<F,S> {
+    public F first;
+    public S second;
+
+    public Pair(F first,S second){
+        this.first= first;
+        this.second = second;
+    }
+
+    public void setFirst(F input){
+        this.first = input;
+    }
+
+    public void setSecond(S input){
+        this.second = input;
+    }
+
+    public F getFirst(){
+        return this.first;
+    }
+
+    public S getSecond(){
+        return this.second;
+    }
+    
+}
