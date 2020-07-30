0d3e1a96d6a1ba9dbfe84ee8f0f361568c0e9fe7
vitaliy <vkovalevskiy@omilia.com>
2020-07-30 14:04:58 +0300

    created JdbcInsert and JdbcRetrieve classes

---
+++ src/main/java/lesson_12/JdbcInsert.java
@@ -0,0 +1,45 @@
+package lesson_12;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JdbcInsert {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/test?" +
+                "useSSl=false?" +
+                "useUnicode=true&" +
+                "useJDBCCompliantTimezoneShift=true&" +
+                "useLegacyDatetimeCode=false&" +
+                "serverTimezone=UTC";
+        String user = "root";
+        String password = "root";
+
+        int id = 1;
+        String userName = "Bob";
+        String userEmail = "bob@bob.com";
+        String sql = "INSERT INTO User(Id, Name, Email) VALUES(?, ?, ?)";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setInt(1, id);
+            pst.setString(2, userName);
+            pst.setString(3, userEmail);
+            pst.executeUpdate();
+
+            System.out.println("A new user has been inserted");
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(JdbcInsert.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        }
+    }
+}
---
+++ src/main/java/lesson_12/JdbcRetrieve.java
@@ -0,0 +1,41 @@
+package lesson_12;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JdbcRetrieve {
+
+    public static void main(String[] args) {
+
+
+        String url = "jdbc:mysql://localhost:3306/test?" +
+                "useSSl=false?" +
+                "useUnicode=true&" +
+                "useJDBCCompliantTimezoneShift=true&" +
+                "useLegacyDatetimeCode=false&" +
+                "serverTimezone=UTC";
+        String user = "root";
+        String password = "root";
+
+        String query = "SELECT * FROM user where name = 'Bob'";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement pst = con.prepareStatement(query);
+             ResultSet rs = pst.executeQuery()) {
+
+            while (rs.next()) {
+
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.println(rs.getString(2));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(JdbcRetrieve.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
+
